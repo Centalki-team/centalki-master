5400a484fe296d93a540901a5492dca94dc3b99c
NKV <kha-vi.nguyen@spiritlabs.co>
2022-12-10 03:54:23 +0700

    updtae port

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(3000);
-  console.log(`Centalki Master listening on http port ${3000}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Centalki Master listening on http port ${port}`);
 }
 bootstrap();
