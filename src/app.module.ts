850c274e7eb9d2221e58faa2b7a4b0db5e691499
NKV <kha-vi.nguyen@spiritlabs.co>
2022-12-10 23:31:24 +0700

    env adhoc

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,9 @@ const LOCAL_PATH =
   '/Users/admin/Documents/centalki/centalki-master/service-account-file.json';
 console.log(`Env: ${process.env.NODE_ENV}`);
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const accountService = require(RENDER_PATH);
+const accountService = require(process.env.NODE_ENV === 'production'
+  ? RENDER_PATH
+  : LOCAL_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(accountService),
