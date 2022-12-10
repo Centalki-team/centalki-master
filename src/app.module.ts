d5b8f22bd845b08c71353aca87b63cc779b55143
NKV <kha-vi.nguyen@spiritlabs.co>
2022-12-10 23:04:59 +0700

    create index

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,17 @@ import { CategoryModule } from './category/category.module';
 import { TopicModule } from './topic/topic.module';
 import { QuestionModule } from './question/question.module';
 import { PhraseModule } from './phrase/phrase.module';
+const RENDER_PATH = '/etc/secrets/service-account-file.json';
+const LOCAL_PATH =
+  '/Users/admin/Documents/centalki/centalki-master/service-account-file.json';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const accountService = require(RENDER_PATH);
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(accountService),
   storageBucket: 'centalki-staging.appspot.com',
 });
+const firestore = admin.firestore();
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -27,6 +33,7 @@ admin.initializeApp({
     CommonModule,
     FireormModule.forRoot({
       fireormSettings: { validateModels: false },
+      firestore,
     }),
     AuthModule,
     StorageModule,
