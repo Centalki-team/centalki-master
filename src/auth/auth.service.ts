124f22d67139f3c7194c7f831b15588cd86b6ccc
NKV <nguyenkhavi01032001@gmail.com>
2023-02-21 10:52:55 +0700

    fix api device token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -273,7 +273,9 @@ export class AuthService {
   async addDeviceToken(user: UserRecord, dto: SetDeviceTokenDto) {
     const uid = user.uid;
     const exist = await this.authCollection.whereEqualTo('uid', uid).findOne();
-    exist.deviceTokens = [...exist.deviceTokens, dto.token];
+    exist.deviceTokens = exist.deviceTokens
+      ? [...exist.deviceTokens, dto.token]
+      : [dto.token];
     return await this.authCollection.update(exist);
   }
 
